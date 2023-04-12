80bd75f15900d102a2dc9d1572c4b4991249c64b
mehul025 <112761617+mehul025@users.noreply.github.com>
2023-04-12 12:14:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
-resource "aws_instance" "weblogic" {
+resource "aws_instance" "logggic" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
